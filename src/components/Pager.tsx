845c3ad9b30d1efef7e24dff95a1ac499814e230
Lawrence Onah <paplow01@gmail.com>
2022-11-12 18:53:35 +0100

    Added auth guard

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -1,7 +1,19 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { IRoute } from "../routes";
 import Spinner from "./Spinner";
 
 export default function Pager(props: IRoute) {
+  const navigate = useNavigate();
+  const { pathname, search } = useLocation();
+
+  // Auth guard
+  useEffect(() => {
+    // Check auth status and redirect if user is a guest
+    if (props.protected /* && isGuestUser */) {
+      navigate({ pathname: "/" }, { state: { from: pathname + search } });
+    }
+  }, [pathname, search]);
+
   return <Suspense fallback={<Spinner />}>{props.element}</Suspense>;
 }
