5ad6b2d9ad8cc49eda9fe5ef79f1f682682ef232
Lawrence Onah <paplow01@gmail.com>
2022-11-13 06:45:53 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // https://github.com/trapcodeio/vite-plugin-ejs
     ViteEjsPlugin((viteConfig) => ({
       // viteConfig is the current Vite resolved config
       env: viteConfig.env,
     })),
+
+    // https://github.com/vitejs/vite-plugin-react
     react(),
   ],
 });
