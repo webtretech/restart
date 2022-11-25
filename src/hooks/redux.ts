5224fcea6d9c81d0f1fb9d2bc17991430252e181
Lawrence Onah <paplow01@gmail.com>
2022-11-25 13:48:11 +0100

    refactored to adhere to style guide

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,5 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "../redux";
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
