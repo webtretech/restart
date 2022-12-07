6fef116f468db36865d6f360e970c81cd50af56a
Lawrence Onah <paplow01@gmail.com>
2022-12-07 12:35:16 +0100

    chore: improved typing

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,6 +1,7 @@
+import { AnyAction, Dispatch } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "../redux";
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = (): Dispatch<AnyAction> =>
+  useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
