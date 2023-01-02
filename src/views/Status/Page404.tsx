353bab17b4f1e29265ef7b2c4e2df8da9b9cbe45
Lawrence Onah <paplow01@gmail.com>
2023-01-02 07:39:45 +0100

    chore: add 404 route

---
+++ src/views/Status/Page404.tsx
@@ -0,0 +1,10 @@
+export default function Page404(): JSX.Element {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto text-center">
+        <h1 className="text-7xl font-bold">404</h1>
+        <p className="text-center text-2xl">Page not found</p>
+      </div>
+    </div>
+  );
+}
