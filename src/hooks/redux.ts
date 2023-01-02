9a30d9f660a712eb5dc225ae7d7d313050b6397b
Lawrence Onah <paplow01@gmail.com>
2023-01-02 07:51:47 +0100

    refactor: improve redux typing

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,7 +1,5 @@
-import { AnyAction, Dispatch } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "../redux";
+import type { AppDispatch, RootState } from "../redux";
 
-export const useAppDispatch = (): Dispatch<AnyAction> =>
-  useDispatch<AppDispatch>();
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
