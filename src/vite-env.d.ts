9c4b09275b99f833d9aa00697dd7c5385f010e65
Lawrence Onah <paplow01@gmail.com>
2023-03-01 00:01:53 +0100

    feat: added env validation at runtime

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,6 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_APP_NAME: string;
-  // more env variables...
-}
+import { EnvironmentVariable } from "@/env";
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: EnvironmentVariable;
 }
