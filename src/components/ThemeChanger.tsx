6fa3f4dcb17c7b16de3bace9cae94383bdb38f43
Lawrence Onah <paplow01@gmail.com>
2023-03-17 21:00:16 +0100

    refactor: improved theme changer

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,10 +1,14 @@
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
-import { getDarkTheme } from "@/redux/app/appSelector";
+import { getTheme } from "@/redux/app/appSelector";
 import { appActions } from "@/redux/app/appSlice";
-import { useEffect } from "react";
-import { Swap } from "react-daisyui";
-import { twMerge } from "tailwind-merge";
-import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+import clsx from "clsx";
+import { useEffect, useMemo } from "react";
+import { Button, Dropdown } from "react-daisyui";
+import {
+  MdOutlineBrightnessAuto,
+  MdOutlineDarkMode,
+  MdOutlineLightMode,
+} from "react-icons/md";
 import { themeChange } from "theme-change";
 
 export default function ThemeChanger({
@@ -13,31 +17,64 @@ export default function ThemeChanger({
   className?: string;
 }): JSX.Element {
   const dispatch = useAppDispatch();
-  const darkTheme = useAppSelector(getDarkTheme);
+  const theme = useAppSelector(getTheme);
+  const themeIcon = useMemo(() => {
+    switch (theme) {
+      case "light":
+        return <MdOutlineLightMode className="h-5 w-5" />;
+
+      case "dark":
+        return <MdOutlineDarkMode className="h-5 w-5" />;
+
+      default: {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          const html = document.querySelector("html") as HTMLElement;
+          html.setAttribute("data-theme", "night");
+        }
+
+        return <MdOutlineBrightnessAuto className="h-5 w-5" />;
+      }
+    }
+  }, [theme]);
 
   useEffect(() => {
     themeChange(false);
   }, []);
 
   return (
-    <Swap
-      rotate
-      active={darkTheme}
-      className={twMerge("btn-ghost btn-circle btn", className)}
-      onElement={
-        <MdOutlineLightMode
-          className="h-5 w-5"
+    <Dropdown vertical="end" className={className}>
+      <Dropdown.Toggle button={false}>
+        <Button shape="circle" color="ghost">
+          {themeIcon}
+        </Button>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className="menu-compact mt-3 w-32 p-2">
+        <Dropdown.Item
+          data-set-theme=""
+          className={clsx({ bordered: theme === "auto" })}
+          onClick={() => dispatch(appActions.setTheme("auto"))}
+        >
+          <MdOutlineBrightnessAuto className="h-5 w-5" /> Auto
+        </Dropdown.Item>
+
+        <Dropdown.Item
           data-set-theme="night"
-          onClick={() => dispatch(appActions.toggleDarkTheme())}
-        />
-      }
-      offElement={
-        <MdOutlineDarkMode
-          className="h-5 w-5"
+          className={clsx({ bordered: theme === "dark" })}
+          onClick={() => dispatch(appActions.setTheme("dark"))}
+        >
+          <MdOutlineDarkMode className="h-5 w-5" /> Dark
+        </Dropdown.Item>
+
+        <Dropdown.Item
           data-set-theme="light"
-          onClick={() => dispatch(appActions.toggleDarkTheme())}
-        />
-      }
-    />
+          className={clsx({ bordered: theme === "light" })}
+          onClick={() => dispatch(appActions.setTheme("light"))}
+        >
+          <MdOutlineLightMode className="h-5 w-5" />
+          Light
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
