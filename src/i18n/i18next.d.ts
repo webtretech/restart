78a995cd048ff1434d38aa417b82f5a0671c8298
Lawrence Onah <paplow01@gmail.com>
2023-03-23 19:21:27 +0100

    chore(i18n): improved typing

--- src/i18n/i18next.d.ts
+++ src/i18n/i18next.d.ts
@@ -1,8 +1,9 @@
-import { resources } from ".";
+import { type resources } from "@/i18n";
 
 declare module "i18next" {
   interface CustomTypeOptions {
     returnNull: false;
+    // eslint-disable-next-line prettier/prettier
     resources: typeof resources["en-US"];
   }
 }
