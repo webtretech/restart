beb17b6fa6e54c19a94f79c6227af210d6ccbb0c
Lawrence Onah <paplow01@gmail.com>
2023-07-25 16:04:11 +0100

    refactor(commitlint.config.cjs): remove unnecessary rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,6 @@
-const defaultConfig = require("@commitlint/config-conventional");
-
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    ...defaultConfig.rules,
     "type-enum": [
       2,
       "always",
