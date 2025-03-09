64666edf3e572fb1ee0dff42d901c034f6876bce
Lawrence Onah <paplow01@gmail.com>
2025-03-09 11:54:52 +0100

    build: update dependencies and configure Vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,50 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig, PluginOption } from "vite";
+import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const plugins: PluginOption[] = [
+    react(),
+    svgr(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/**.{ts,tsx}"',
+        useFlatConfig: true,
+        dev: {
+          logLevel: ["error", "warning"],
+        },
+      },
+    }),
+    visualizer({
+      emitFile: true,
+      brotliSize: true,
+      template: "treemap",
+      filename: "stats.html",
+    }),
+  ];
+
+  if (mode === "development") {
+    plugins.push(basicSsl());
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
     },
-  },
-  build: {
-    sourcemap: true,
-    reportCompressedSize: false,
-  },
+    server: {
+      open: true,
+    },
+    build: {
+      sourcemap: true,
+      reportCompressedSize: false,
+    },
+  };
 });
