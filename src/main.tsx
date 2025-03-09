0d7b1ebf7dc03332ffdfbbfd9315b4d516c07c81
Lawrence Onah <paplow01@gmail.com>
2025-03-09 11:08:42 +0100

    refactor: simplify error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import App from "@/App";
-import ErrorFallback from "@/components/ErrorFallback";
 import store from "@/store";
 import "@/style.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ErrorBoundary } from "react-error-boundary";
 import { Provider as ReduxProvider } from "react-redux";
-
-// Global error handler
-const errorHandler = (error: Error, info: { componentStack: string }): void => {
-  // You can log errors to third-party services
-  if (import.meta.env.DEV) console.error(error, info);
-};
 
 // Create app
 const root = createRoot(document.getElementById("root")!);
@@ -19,10 +11,8 @@ const root = createRoot(document.getElementById("root")!);
 // Render the app
 root.render(
   <StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback} onError={errorHandler}>
-      <ReduxProvider store={store}>
-        <App />
-      </ReduxProvider>
-    </ErrorBoundary>
-  </StrictMode>
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </StrictMode>,
 );
--- src/router/interface.ts
+++ src/router/interface.ts
@@ -2,11 +2,6 @@ import { type RouteObject } from "react-router-dom";
 
 export type AppRouteObject = RouteObject & {
   /**
-   * @description routes that need login
-   */
-  isProtected?: boolean;
-
-  /**
    * @description child route
    */
   children?: AppRouteObject[];
